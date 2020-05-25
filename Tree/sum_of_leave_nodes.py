70523795c3ef2a232cbafc27b1acb2be0b6b9968
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-05-25 18:58:24 +0530

    some easy problems

---
+++ Tree/sum_of_leave_nodes.py
@@ -0,0 +1,66 @@
+def sumLeaf(root):
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return root.data
+    return sumLeaf(root.left) + sumLeaf(root.right)
+
+from collections import deque
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+   
+def buildTree(s):
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+
+    ip=list(map(str,s.split()))
+
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+  
+    q.append(root)                            
+    size=size+1 
+
+    i=1                                       
+    while(size>0 and i<len(ip)):
+ 
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+
+        currVal=ip[i]
+
+        if(currVal!="N"):
+            
+  
+            currNode.left=Node(int(currVal))
+            
+            
+            q.append(currNode.left)
+            size=size+1
+
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        if(currVal!="N"):
+
+            currNode.right=Node(int(currVal))
+
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        print(sumLeaf(root))
