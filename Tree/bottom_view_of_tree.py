2e7e9d3072527be4a1c99ed139f6d98781955aba
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-06-08 22:57:10 +0530

    problema based on trees

---
+++ Tree/bottom_view_of_tree.py
@@ -0,0 +1,73 @@
+def bottom(root,h,l,d):
+    if root is None:
+        return
+    if h not in d or l >= d[h][1]:
+        d[h] = (root.data,l)
+    bottom(root.left,h-1,l+1,d)
+    bottom(root.right,h+1,l+1,d)
+
+def bottomView(root):
+    d = {}
+    bottom(root,0,0,d)
+    for key in sorted(d.keys()):
+        print(d.get(key)[0],end=' ')
+        
+from collections import deque
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+   
+def buildTree(s):
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+
+    ip=list(map(str,s.split()))
+
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+  
+    q.append(root)                            
+    size=size+1 
+
+    i=1                                       
+    while(size>0 and i<len(ip)):
+ 
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+
+        currVal=ip[i]
+
+        if(currVal!="N"):
+            
+  
+            currNode.left=Node(int(currVal))
+            
+            
+            q.append(currNode.left)
+            size=size+1
+
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        if(currVal!="N"):
+
+            currNode.right=Node(int(currVal))
+
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        bottomView(root)
+        print()
