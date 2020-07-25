8c10d31c3a8000a65f9b65feef6f3491dc101bd7
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-07-25 21:49:37 +0530

    leetcode

---
+++ Tree/symmetric_tree.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(root1,root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is not None and root2 is not None:
+                if root1.val == root2.val:
+                    return isMirror(root1.left,root2.right) and isMirror(root1.right,root2.left)
+        return isMirror(root,root)
