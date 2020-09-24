c608877870979d27bf8efab6f09537b5f53e0915
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-09-24 17:33:39 +0530

    correction made

--- Dynamic programming/longest_bitonic_subsequence.py
+++ Dynamic programming/longest_bitonic_subsequence.py
@@ -11,26 +11,28 @@ def insr():
     return(list(s[:len(s) - 1]))
 def invr():
     return(map(int,input().split()))
-class Solution:
-	def LongestBitonicSequence(self, arr):
-        n = len(arr)
-        dp = [1 for i in range(n)]
-        for i in range(1,n):
-            for j in range(i):
-                if arr[i] > arr[j]:
-                    dp[i] = max(dp[i],1 + dp[j])
-        ddp = [1 for i in range(n)]
-        for i in range(n-2,-1,-1):
-            for j in range(n-1,i,-1):
-                if arr[i] > arr[j]:
-                    ddp[i] = max(ddp[i],1 + ddp[j])
-        mx = dp[0] + ddp[0] - 1
-        for i in range(1,n):
-            mx = max(mx,dp[i] + ddp[i] - 1)
-        return mx
+
+
+def LongestBitonicSequence(arr):
+    n = len(arr)
+    dp = [1 for i in range(n)]
+    for i in range(1,n):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i],1 + dp[j])
+    ddp = [1 for i in range(n)]
+    for i in range(n-2,-1,-1):
+        for j in range(n-1,i,-1):
+            if arr[i] > arr[j]:
+                ddp[i] = max(ddp[i],1 + ddp[j])
+    mx = dp[0] + ddp[0] - 1
+    for i in range(1,n):
+        mx = max(mx,dp[i] + ddp[i] - 1)
+    return mx
+
 t = inp()
 while t > 0:
     arr = inlt()
     n = len(arr)
-    print(fun(arr,n))
+    print(LongestBitonicSequence(arr))
     t -= 1
