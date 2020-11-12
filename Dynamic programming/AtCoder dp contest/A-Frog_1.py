83238a6ece56ba01f9ba3f3664e10b6dc8105530
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-11-12 21:53:24 +0530

    dp problems atcoder

---
+++ Dynamic programming/AtCoder dp contest/A-Frog_1.py
@@ -0,0 +1,12 @@
+def helper(arr,n):
+    if n == 2:
+        return abs(arr[1] - arr[0])
+    dp = [0 for i in range(n)]
+    dp[1] = abs(arr[1] - arr[0])
+    for i in range(2,n):
+        dp[i] = min(dp[i-2]+abs(arr[i]-arr[i-2]), dp[i-1] + abs(arr[i] - arr[i-1]))
+    return dp[n-1]
+
+n = int(input())
+arr = list(map(int,input().split()))
+print(helper(arr,n))
---
+++ Dynamic programming/AtCoder dp contest/B-Frog_2.py
@@ -0,0 +1,15 @@
+def helper(arr,n,k):
+    if (n==2):
+        return abs(arr[1]-arr[0])
+    dp = [0 for i in range(n)]
+    dp[1] = abs(arr[1] - arr[0])
+    for i in range(2,n):
+        dp[i] = float("inf")
+        for k in range(1,k+1):
+            if i-k >= 0:
+                dp[i] = min(dp[i], dp[i-k] + abs(arr[i] - arr[i-k]))
+    return dp[n-1]
+
+n,k = map(int,input().split())
+arr = list(map(int,input().split()))
+print(helper(arr,n,k))
